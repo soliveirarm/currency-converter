07cc1558091ee43540494a50e598009ce2ef8e95
Sarah Oliveira <sarah.mdeo@outlook.com>
2024-02-05 03:35:55 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const brl = document.getElementById("brl");
+const result = document.getElementById("result");
+
+async function getData() {
+  try {
+    const response = await fetch("https://open.er-api.com/v6/latest/BRL");
+    let result = await response.json();
+    const currencies = {
+      usd: result.rates.USD,
+      eur: result.rates.EUR,
+      gbp: result.rates.GBP,
+    };
+    return currencies;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const formatResult = (currency, result) => `${currency}${result.toFixed(2)}`;
+
+getData().then((data) => {
+  const exchangeRate = document.querySelector("#exchange-rate");
+  exchangeRate.textContent = `R$1 = ${formatResult(
+    "$",
+    data.usd
+  )} - ${formatResult("€", data.eur)} - ${formatResult("£", data.gbp)}`;
+});
+
+function convert() {
+  let brlVal = parseFloat(brl.value).toFixed(2);
+  const currencies = document.querySelector("#currencies");
+
+  if (brl.value !== "") {
+    getData().then((data) => {
+      switch (currencies.value) {
+        case "dollar":
+          result.textContent = formatResult("$", brlVal * data.usd);
+          break;
+        case "euro":
+          result.textContent = formatResult("€", brlVal * data.eur);
+          break;
+        case "pound":
+          result.textContent = c;
+          break;
+      }
+      brl.value = "";
+    });
+  } else {
+    if (result.textContent !== "") {
+      result.textContent = "";
+    }
+  }
+}
+
+document.querySelector("#convert").addEventListener("click", convert);
+brl.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    convert();
+  }
+});
+
+brl.focus();
